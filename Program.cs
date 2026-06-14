de5430b6f3a1924ee505c0ef5b2f9d0b154f8500
Sree hari <Admin@DESKTOP-88VFGC1>
2026-06-14 17:19:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Constructors
+{
+    class SmartBulb
+    {
+        public Boolean IsOn;
+        public int brightness;
+        public SmartBulb() 
+        {
+            IsOn = false;
+            brightness = 0;
+        }
+        public SmartBulb(Boolean IsActuallyOn, int ActualBrightness)
+        {
+            IsOn = IsActuallyOn;
+            brightness = ActualBrightness;
+        }
+        public void ShowStatus()
+        {
+            Console.WriteLine($"The current state of BULB is:{IsOn}, and its Brightness:{brightness}");
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartBulb SmartBulb1 = new SmartBulb(true, 70);
+            SmartBulb1.ShowStatus();
+        }
+    }
+}
